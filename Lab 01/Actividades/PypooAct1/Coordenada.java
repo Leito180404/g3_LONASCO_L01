ac53d7b62b5aa613f74394ad63a243fce39f151e
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-18 16:12:38 -0500

    clases

---
+++ Lab 01/Actividades/PypooAct1/Coordenada.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Coordenada{
+    private double x;
+    private double y;
+
+    public Coordenada(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Coordenada(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordenada(Coordenada c){
+    }
+}
