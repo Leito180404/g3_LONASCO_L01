f942d14081cfc125ff117d5f3c34a6299a7986ad
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-19 22:23:17 -0500

    Update Principal.java

--- Lab 01/Actividades/PypooAct1/Principal.java
+++ Lab 01/Actividades/PypooAct1/Principal.java
@@ -8,6 +8,8 @@ public class Principal {
         double c1 = sc.nextDouble();
         double c2 = sc.nextDouble();
 
-        
+        System.out.println("Ingrese la segunda coordenada de la segunnda esquina del rectangulo ");
+        double c3 = sc.nextDouble();
+        double c4 = sc.nextDouble();
     }
 }
