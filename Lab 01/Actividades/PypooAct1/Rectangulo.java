2a2b892f3a0ed374c7cc616445a395a0645262f1
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-19 21:47:49 -0500

    Update Rectangulo.java

--- Lab 01/Actividades/PypooAct1/Rectangulo.java
+++ Lab 01/Actividades/PypooAct1/Rectangulo.java
@@ -5,7 +5,27 @@ public class Rectangulo {
     private Coordenada esquina2;
 
     public Rectangulo (Coordenada c1, Coordenada c2){
-        setEsquina(c1);
-        setEsquina(c2);
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
+
+    public void setEsquina1 (Coordenada coo){
+        this.esquina1= coo;
+    }
+
+    public void setEsquina2 (Coordenada coo){
+        this.esquina2=coo;
+    }
+
+    public Coordenada getEsquina1 (){
+        return esquina1;
+    }
+
+    public Coordenada getEsquina2 (){
+        return esquina2;
+    }
+
+    public String toString(){
+
     }
 }
