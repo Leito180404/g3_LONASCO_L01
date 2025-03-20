854dee1e23bffc1af449b4c486178371fcd6616f
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-20 15:18:49 -0500

    Update Verificador.java

--- Lab 01/Actividades/PypooAct1/Verificador.java
+++ Lab 01/Actividades/PypooAct1/Verificador.java
@@ -4,5 +4,11 @@ public class Verificador {
         Coordenada a2 = A.getEsquina2();
         Coordenada b1 = B.getEsquina1();
         Coordenada b2 = B.getEsquina2();
+
+        double minrecAx = Math.min(a1.getX(), a2.getX());
+        double maxrecAx = Math.max(a1.getX(), a2.getX());
+        double minrecAy = Math.min(a1.getY(), a2.getY());
+        double maxrecAy = Math.min(a1.getY(), a2.getY());
+        
     }
 }
