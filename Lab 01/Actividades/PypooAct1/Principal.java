a180b9fb0978b4157869c0c4729a142ee0b8456f
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-19 22:40:20 -0500

    Update Principal.java

--- Lab 01/Actividades/PypooAct1/Principal.java
+++ Lab 01/Actividades/PypooAct1/Principal.java
@@ -16,6 +16,14 @@ public class Principal {
         Coordenada c2a = new Coordenada(x2a,y2a);
         Rectangulo A = new Rectangulo(c1a, c2a);
 
+        System.out.println("Ingrese la primera coordenada de una esquina del segundo rectangulo ");
+        double x1b = sc.nextDouble();
+        double y1b = sc.nextDouble();
+
+        System.out.println("Ingrese la segunda coordenada de la segunda esquina del segundo rectangulo ");
+        double x2b = sc.nextDouble();
+        double y2b = sc.nextDouble();
+
         
     }
 
