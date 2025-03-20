46cdce6840c5790c2fb1e5d20bb974fd3a99d0d8
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-20 15:09:16 -0500

    Update Verificador.java

--- Lab 01/Actividades/PypooAct1/Verificador.java
+++ Lab 01/Actividades/PypooAct1/Verificador.java
@@ -1,5 +1,5 @@
 public class Verificador {
-    public static boolean sobrepuesta(Rectangulo r1, Rectangulo r2){
-        return (r1.getEsquina1().getX() >= r2.getEsquina2().getX() ||
+    public static String verificarRectangulos(Rectangulo A, Rectangulo B){
+        Coordenada a1 = A.getEsquina1();
     }
 }
