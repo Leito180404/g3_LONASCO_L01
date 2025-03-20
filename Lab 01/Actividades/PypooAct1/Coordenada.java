ba00d81c625f361ffc89a47a2951e3fd66220795
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-19 21:34:49 -0500

    string

--- Lab 01/Actividades/PypooAct1/Coordenada.java
+++ Lab 01/Actividades/PypooAct1/Coordenada.java
@@ -42,4 +42,9 @@ public class Coordenada{
     static double distancia(Coordenada c1, Coordenada c2){
         return Math.sqrt(Math.pow(c1.x - c2.x, 2) + Math.pow(c1.y - c2.y, 2));
     }
+
+    public String toString(){
+        return "coordenada(" + x +" , " + y + ")";
+    }
+
 }
