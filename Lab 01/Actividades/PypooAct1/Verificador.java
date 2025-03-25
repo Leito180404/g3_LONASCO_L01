8275ced7541312d896071b6265f57162b392dcfa
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-24 22:37:32 -0500

    Update Verificador.java

--- Lab 01/Actividades/PypooAct1/Verificador.java
+++ Lab 01/Actividades/PypooAct1/Verificador.java
@@ -6,26 +6,18 @@ public class Verificador {
         double maxY = Math.max(c1.getY(), c2.getY());
 
         return new double[] {minX, maxX, minY, maxY};
-
-        if ((maxrecAx > minrecBx && maxrecBx > minrecAx && maxrecAy > minrecBy && 
-        maxrecBy > minrecAy)){
-            return "Rectangulos A y B se sobreponen";
-        }
-
-        if ((maxrecAx == minrecBx || maxrecBx == minrecAx) && (maxrecAy > minrecBy && 
-        maxrecBy > minrecAy)
-         || (maxrecAy == minrecBy || maxrecBy == minrecAy) && (maxrecAx > minrecBx && 
-         maxrecBx > minrecAx)){
-            return "Estan juntos los rectangulos";
-         }
-
-         else{
-            return "rectangulso disjuntos";
-         } 
     }
 
     public static boolean esSobrePos(Rectangulo A, Rectangulo B){
-        
+        Coordenada a1 = A.getEsquina1();
+        Coordenada a2 = A.getEsquina2();
+        Coordenada b1 = B.getEsquina1();
+        Coordenada b2 = B.getEsquina2();
+
+        double[] limitesA = calcularLimites(a1, a2);
+        double[] limitesB = calcularLimites(b1, b2);
+
+        return limitesA[1] > limitesB[0] && limitesB[1] > limitesA[0] && limitesA[3] > limitesB[2] && limitesB[3] > limitesA[2];
     }
 
     public static boolean esJunto(Rectangulo A, Rectangulo B){
