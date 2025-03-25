41453dbcead3093b806479f4c964b6c69ee6e6f6
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-24 23:18:32 -0500

    Update ContainerRect.java

--- Lab 01/Actividades/PypooEje1/ContainerRect.java
+++ Lab 01/Actividades/PypooEje1/ContainerRect.java
@@ -1,7 +1,16 @@
 public class ContainerRect {
     private Rectangulo[] rectangulos;
-    private double[] distancias;       
-    private double[] areas;            
-    private int n;                     
-    private int count;    
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+    private int numRec;
+    
+    public ContainerRect(int maxRectangulos) {
+        this.n = maxRectangulos;
+        this.rectangulos = new Rectangulo[n];
+        this.distancias = new double[n];
+        this.areas = new double[n];
+        this.numRec = 0;
+    }
+   
 }
